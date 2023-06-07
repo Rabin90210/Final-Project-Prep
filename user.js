4f0446cc3c8e29e1bab88b3e6867cf2c6a432acb
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-06-07 12:28:31 +1000

    A

--- user.js
+++
@@ -1,8 +0,0 @@
-const isEqualTo100 = (x,y) => a === 100 || b === 100
-const isEqualTo100 = (x,y) => a === 100 || b === 100
-const isEqualTo100 = (x,y) => a === 100 || b === 100
-const isEqualTo100 = (x,y) => a === 100 || b === 100
-const isEqualTo100 = (x,y) => a === 100 || b === 100
-const isEqualTo100 = (x,y) => a === 100 || b === 100
-const isEqualTo100 = (x,y) => 1 === 100 || b === 100
-const isEqualTo100 =
